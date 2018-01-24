138e4db06ec245cf10f16395b49368b97c59051c
Xela1988 <a.guernelli@libero.it>
2018-01-24 19:51:48 +0100

    Modifica per aggiunta buttonSave e buttonLoad per salvare e caricare con sqlite e relativi xml, modifiche effettuate anche al manifest per le relative pagine home scelta ecc. con relativi xml e classi

--- app/src/main/java/com/example/alex/newfifteen/MainActivity.java
+++ app/src/main/java/com/example/alex/newfifteen/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.alex.newfifteen;
 
 
         import android.annotation.SuppressLint;
+        import android.app.Activity;
+        import android.content.Intent;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.util.Log;
@@ -16,105 +18,31 @@ package com.example.alex.newfifteen;
 
         import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final int n = 4; // размерность поля (campo dimensione): n*n
-    private Button[] buttons; //массив ячеек (array di celle)
-    private int indexEmpty; //индекс пустой ячейки (indice cella vuota)
-    private int indexes[]; //хранит индексы ячеек (negozi indici cella)
-    private int countPressBtn = 0; //хранит кол-во ходов (memorizza i numero di colpi)
-    private TextView textView; //выводит кол-во ходов (esso mostra il numero di colpi)
+public class MainActivity extends Activity {
+    Button buttonHomeNewGame;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttons = new Button[n * n];
-        indexes = new int[n * n];
-        int[] numbers = RandomOrder.getShuffleArray(0, n * n, n); //массив с числами в случайном порядке (una serie di numeri casuali)
+        buttonHomeNewGame= (Button)findViewById(R.id.buttonHomeNewGame);
 
-        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+        buttonHomeNewGame.setOnClickListener( new View.OnClickListener()
+        {
 
-        Button btn;
-        for (int i = 0; i < n * n; i++) {
-            btn = (Button) gridLayout.getChildAt(i);
-            if (numbers[i] == 0) {
-                btn.setVisibility(View.INVISIBLE);
-                indexEmpty = i;
-            }
-            indexes[numbers[i]] = i;
-            buttons[i] = btn;
-            btn.setText(String.valueOf(numbers[i]));
-            btn.setOnClickListener(this);
-        }
+            @Override
+            public void onClick(View v) {
 
-        textView = (TextView) findViewById(R.id.textView);
+                // Intent code for open new activity through intent.
+
+                Intent intent = new Intent(MainActivity.this, Scelta.class);
+            //    Intent intent1= new Intent(MainActivity.this, Case5.class);
+                startActivity(intent);
+             //   startActivity(intent1);
+
+            }});
     }
 
-    @Override
-    public void onClick(View v) {
-        //ячейка, которая была нажата (cella che è stato cliccato)
-        int number = Integer.parseInt(((Button) v).getText().toString());
-
-        //индекс ячейки (indice delle celle)
-        int idxPressBtn = indexes[number];
-
-        if (((idxPressBtn == indexEmpty - 1) & ((idxPressBtn + 1) % n != 0)) ||
-                ((idxPressBtn == indexEmpty + 1) & (idxPressBtn % (n) != 0)) ||
-                (idxPressBtn == indexEmpty - n) || (idxPressBtn == indexEmpty + n)) {
-
-            swapButton(buttons[idxPressBtn], buttons[indexEmpty]);
-
-            indexes[number] = indexEmpty;
-            indexEmpty = idxPressBtn;
-
-            if (isSolve()) {
-                Toast.makeText(this, "You solved it!", Toast.LENGTH_LONG).show();
-            } else
-                textView.setText("Number of moves: " + (++countPressBtn));
-        }
-    }
-
-    private void swapButton(Button pressButton, Button emptyButton) {
-        emptyButton.setText(pressButton.getText());
-        emptyButton.setVisibility(View.VISIBLE);
-
-        pressButton.setVisibility(View.INVISIBLE);
-    }
-
-    @SuppressLint("SetTextI18n")
-    private void startNewGame() {
-        int[] numbers = RandomOrder.getShuffleArray(0, n * n, n);
-        buttons[indexEmpty].setVisibility(View.VISIBLE);
-        for (int i = 0; i < n * n; i++) {
-            indexes[numbers[i]] = i;
-            if (numbers[i] != 0)
-                buttons[i].setText(String.valueOf(numbers[i]));
-            else {
-                buttons[i].setVisibility(View.INVISIBLE);
-                indexEmpty = i;
-            }
-        }
-        textView.setText("Number of moves: " + (countPressBtn = 0)); //обнулить счетчик (azzera il  contatore)
-    }
-
-    private boolean isSolve() {
-        for (int i = 1; i < indexes.length - 1; i++)
-            if (indexes[i] != i - 1) return false;
-
-        return true;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.mymenu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        startNewGame();
-        return super.onOptionsItemSelected(item);
-    }
 }
